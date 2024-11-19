519ba69559f8f8d81151ec03051d056551b14fe8
KultyshevaEkaterina <144539184+KultyshevaEkaterina@users.noreply.github.com>
2024-11-19 21:36:50 +0500

    Task2

---
+++ TodoServerApp/Data/Services/MemoryDataService.cs
@@ -0,0 +1,19 @@
+﻿using TodoServerApp.Data.Interfaces;
+namespace TodoServerApp.Data
+{
+    public class MemoryDataService: IDataService
+    {
+        private static IEnumerable<TaskItem> tasks = new List<TaskItem>
+        {
+
+        new () {Id=1,Title="Задача 1", Description="Описание задачи 1", CreateDate=DateTime.Now},
+        new () {Id=1,Title="Задача 2", Description="Описание задачи 2", CreateDate=DateTime.Now},
+        new () {Id=1,Title="Задача 3", Description="Описание задачи 3", CreateDate=DateTime.Now},
+        };
+        public async Task<IEnumerable<TaskItem>> GetTaskItemAsync()
+        {
+            await Task.Delay(1000);
+            return await Task.FromResult(tasks);
+        }
+    }
+}
